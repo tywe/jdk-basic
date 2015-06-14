e1b1fe5bab8b52fd296d681e0b110650cfa5a752
baichuan <tywei@qq.com>
2015-06-14 21:43:43 +0800

    打断点

---
+++ class-reflect/src/org/teng/jdk/reflect/basic/BootStrapTest.java
@@ -0,0 +1,16 @@
+package org.teng.jdk.reflect.basic;
+
+import java.net.URL;
+
+/**
+ * Created by Administrator on 6/14/15.
+ */
+public class BootStrapTest {
+    public static void main(String[] args) {
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+
+        for(int i = 0; i < urls.length; i++){
+            System.out.println(urls[i].toExternalForm());
+        }
+    }
+}
